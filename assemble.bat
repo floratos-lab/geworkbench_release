34628d2f7c2cc70dc2179b5c5b38a39fde746fb7
Kenneth Smith <ksmith@c2b2.columbia.edu>
2014-12-24 17:33:00 -0500

    update assemble.bat to copy demandScripts directory correctly

--- assemble.bat
+++ assemble.bat
@@ -2,3 +2,4 @@ XCOPY  /S /I geworkbench-core geworkbench_dev
 XCOPY  /S /I geworkbench-components\components geworkbench_dev\components
 COPY geworkbench_supporting_files\cytoscape\cytoscape.jar geworkbench_dev\components\cytoscape\lib\cytoscape.jar
 COPY geworkbench_supporting_files\viper\viper.tar.gz geworkbench_dev\components\viper\viperScripts\viper.tar.gz
+XCOPY /S /I demandScripts geworkbench_dev\components\demand\demandScripts
